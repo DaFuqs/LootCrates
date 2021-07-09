e48ad9d771192bc995e4d49aa4a6b8cbae2ffa4d
DaFuqs <dominik.m.schmidt@googlemail.com>
2021-07-09 09:10:15 +0200

    Add config

--- src/main/java/de/dafuqs/lootcrates/config/LootCratesConfig.java
+++ src/main/java/de/dafuqs/lootcrates/config/LootCratesConfig.java
@@ -2,7 +2,6 @@ package de.dafuqs.lootcrates.config;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
-import me.shedaniel.autoconfig.annotation.ConfigEntry;
 
 @Config(name = "LootCrates")
 public class LootCratesConfig implements ConfigData {
